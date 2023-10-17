5635d92c9d9e1708d6085e9a6c4a96292595c310
LukIt4InDr <lukIt4.gabriel@gmail.com>
2023-10-17 14:31:57 -0300

    login

--- ventana-de-inicio-de-sesion/script.js
+++ ventana-de-inicio-de-sesion/script.js
@@ -1,5 +1,29 @@
 window.onload = inicio;
 
 function inicio(){
+    let loginForm = document.getElementById("login-form");
+    loginForm.addEventListener("submit", iniciarSesion);
+}
+
+function iniciarSesion(evento){
+    if(validarInicio()){
+
+    } else{
+        evento.preventDefault();
+    }
+}
+
+function validarInicio(){
+    let userH = document.getElementById("user");
+    let userV = userH.value;
+
+    let pwdH = document.getElementById("pwd");
+    let pdwV = pwdH.value;
     
+    console.log(userV);
+    if(!(userV === "Lucas" || userV === "Maria" || userV === "Daniel")){
+        return false;
+    }
+    
+    return true;
 }
